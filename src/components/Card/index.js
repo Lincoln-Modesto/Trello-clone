e49923d3644f298b867a08d7028a9eb5fef3e9e0
lincoln <lincoln-md@hotmail.com>
2021-11-07 16:33:46 -0300

    finalizado drag-and-drop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,8 @@
 import { useDispatch } from "react-redux"
+import { Draggable } from "react-beautiful-dnd";
 import { setCard, setModal } from "../../store/modules/task/actions";
 
-const Card = ({ id, title, description, columnId }) => {
+const Card = ({ id, title, description, columnId, index }) => {
 
   const dispatch = useDispatch();
 
@@ -18,17 +19,28 @@ const Card = ({ id, title, description, columnId }) => {
   })
 
   return (
-    <div class="card mt-2" onClick={() => {
-      dispatch(actionSetCard)
-      dispatch(actionSetModal)
-    }}>
-      <div class="card-body">
-        <h5 class="card-title">
-          <span class="badge badge-secondary">-</span> {title}
-        </h5>
-        <small class="card-text">{description}</small>
-      </div>
-    </div>
+    <Draggable
+      draggableId={id?.toString()}
+      index={index}>
+      {(provided) =>
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          class="card mt-2"
+          onClick={() => {
+            dispatch(actionSetCard)
+            dispatch(actionSetModal)
+          }}>
+          <div class="card-body">
+            <h5 class="card-title">
+              <span class="badge badge-secondary">-</span> {title}
+            </h5>
+            <small class="card-text">{description}</small>
+          </div>
+        </div>}
+    </Draggable>
+
   )
 }
 
